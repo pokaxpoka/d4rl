edd302943aa8019d133fbbff3fcf16abf683b6e4
young-geng <young.gengxy@gmail.com>
2021-11-08 17:03:02 -0800

    set DM control as optional dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from platform import platform
 
 from setuptools import find_packages
 
+extras = {}
+extras['dm_control'] = [
+    'dm_control' if 'macOS' in platform() else
+    'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control'
+]
+
 setup(
     name='d4rl',
     version='1.1',
@@ -13,9 +19,8 @@ setup(
                       'h5py',
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+    extras_require=extras,
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
